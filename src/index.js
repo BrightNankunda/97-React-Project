f5ed25ad15639efff5778ffa2019f6403b8e54f6
BrightNankunda <67958069+BrightNankunda@users.noreply.github.com>
2021-01-24 10:06:21 +0300

    My second Commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Portfolio from './Portfolio/Body/portfolio';
+// import Portfolio from './Portfolio/Body/portfolio';
 // import App from './App';
 // import Fashionx from './fashionx';
-//import Todoo from './components/Main/Todoo';
+import Todoo from './components/Main/Todoo';
 
 
 
 
 ReactDOM.render(
-    <Portfolio />
+    <Todoo />
     ,
   document.getElementById('root')
 );
